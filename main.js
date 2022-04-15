7105c7a0e016b93a698c30144d102ec4203e6fe1
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jankee112@freenet.de>
2022-04-15 16:41:15 +0200

    sorting variables

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-let volume = document.getElementById
+let min = document.getElementById("min").innerText; //* '0 GB';
+let max = document.getElementById("max").innerText; //* '1000 GB';
+let usage = document.getElementById("usage").innerText; //* '815 GB'
+let rest = document.getElementById("rest").innerText; //* 185;
