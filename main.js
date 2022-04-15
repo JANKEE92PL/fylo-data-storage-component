3390a4accd097e3145975495f31f0ced62299903
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jankee112@freenet.de>
2022-04-15 17:08:01 +0200

    finished project

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
 let min = document.getElementById("min").innerText; //* '0 GB';
 let max = document.getElementById("max").innerText; //* '1000 GB';
-let usage = document.getElementById("usage").innerText; //* '815 GB'
-let rest = document.getElementById("rest").innerText; //* 185;
+
+let storage = {};
+
+const checkUsage = () => {
+  let usage = document.getElementById("usage").innerText; //* '815 GB'
+  storage.usage = usage;
+};
+
+const checkRest = () => {
+  let rest = document.getElementById("rest").innerText; //* 185;
+  storage.rest = rest;
+};
+
+const usageWarning = (() => {
+  checkUsage();
+  checkRest();
+  if (storage.rest < 200) {
+    console.info("almost full space");
+  }
+})();
